df58e64183e215d94eb289976ef7534deaef761b
twqueen <tiffanyytsai@gmail.com>
2023-05-22 21:25:56 -0700

    added paths to server/server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.use(routes)
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server now running on ${PORT}`);
+    });
+});
