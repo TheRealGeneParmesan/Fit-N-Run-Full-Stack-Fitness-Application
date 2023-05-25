b0be617c3e08a0c93cf901dd02e17032c9acf7e6
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-25 11:04:24 -0700

    commented out auth/login to style profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,28 +1,29 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_USER } from "../utils/queries";
-import Auth from "../utils/auth";
+// import { useQuery } from "@apollo/react-hooks";
+// import { QUERY_USER } from "../utils/queries";
+// import Auth from "../utils/auth";
 import { Container } from "react-bootstrap";
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 
 
 const Profile = () => {
-    const { loading, data } = useQuery(QUERY_USER);
-    const user = data?.user || {};
+    // const { loading, data } = useQuery(QUERY_USER);
+    // const user = data?.user || {};
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
 
-    if (!Auth.loggedIn()) {
-        return <Redirect to="/login" />;
-    }
+    // if (!Auth.loggedIn()) {
+    //     return <Redirect to="/login" />;
+    // }
 
     return (
-        <div classname="my fithub">
+        <div className="my fithub">
             <Container className="fitContainer" >
                 <h1 className="fitTitle">My FitHub</h1>
-                <p className="fitText">Welcome to your FitHub, {user.username}! Let's get those gains! And then maybe celebrate with a donut? 🍩 A gluten free one of course! Made out of cauliflower!
+                {/* Add user.username */}
+                <p className="fitText">Welcome to your FitHub! Let's get those gains! And then maybe celebrate with a donut? 🍩 A gluten free one of course! Made out of cauliflower!
                 </p>
             </Container>
         </div>
