48ab5902b93c11568c3340df8e48a8f555c76571
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-05-24 20:33:43 -0700

    Create strength.js
    added strength model

---
+++ server/models/strength.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const StrengthSchema = new Schema(
+  {
+    type: {
+      type: String, 
+      default: "strength",
+      required: true
+    },
+    name: {
+      type: String,
+      required: true,
+      maxlength: 25
+    },
+    weight: {
+      type: Number,
+      required: false
+    },
+    sets: {
+      type: Number,
+      required: true
+    },
+    reps: {
+      type: Number,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+  }
+);
+
+const Strength = model("Strength", StrengthSchema);
+
+module.exports = Strength;
