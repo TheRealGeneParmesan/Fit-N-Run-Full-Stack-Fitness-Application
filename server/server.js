244c83eadc10380e5bca7e9a37706eb17f0fd6ff
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-26 11:59:58 -0700

    added auth middleware to server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
 })
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,12 +25,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes)
 
 const startApolloServer = async () => {
     await server.start();
     server.applyMiddleware({ app });
-    
+
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}!`);
@@ -38,4 +38,4 @@ const startApolloServer = async () => {
     })
 };
 
-startApolloServer()
+startApolloServer();
