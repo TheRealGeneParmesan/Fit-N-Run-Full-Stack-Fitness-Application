b992c860c0e85e6afc0f688a5bd29b6130ce4c38
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-27 19:49:14 -0700

    finished styling cardio & strength

--- client/src/pages/strength.js
+++ client/src/pages/strength.js
@@ -7,6 +7,7 @@ const Strength = () => {
     const [reps, setReps] = useState('');
     const [sets, setSets] = useState('');
     const [weight, setWeight] = useState('');
+    const [date, setDate] = useState('');
 
     const handleActivityChange = (e) => {
         setActivity(e.target.value);
@@ -24,37 +25,47 @@ const Strength = () => {
         setWeight(e.target.value);
     };
 
+    const handleDateChange = (e) => {
+        setDate(e.target.value);
+    };
+
     const handleStrengthSubmit = (e) => {
         e.preventDefault();
+
 
         setActivity('');
         setReps('');
         setSets('');
         setWeight('');
+        setDate('');
     };
 
     return (
         <div className="strengthImg" style={{ backgroundImage: `url(${strengthBg})` }}>
             <Container
                 classname="strengthContainer" >
-                <h1 className="strengthTitle"> Strength Training </h1>
                 <div className="strengthForm">
+                    <h1 className="strengthTitle"> Strength Training </h1>
                     <form onSubmit={handleStrengthSubmit}>
-                        <div className="form-group">
+                        <div className="form-group label">
                             <label>Activity:</label>
                             <input type="text" className="form-control" value={activity} onChange={handleActivityChange} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group label">
                             <label>Reps:</label>
                             <input type="text" className="form-control" value={reps} onChange={handleRepsChange} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group label">
                             <label>Sets:</label>
                             <input type="text" className="form-control" value={sets} onChange={handleSetsChange} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group label">
                             <label>Weight:</label>
                             <input type="text" className="form-control" value={weight} onChange={handleWeightChange} />
+                        </div>
+                        <div className="form-group label">
+                            <label>Date</label>
+                            <input type="text" className="form-control" value={date} onChange={handleDateChange} />
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
