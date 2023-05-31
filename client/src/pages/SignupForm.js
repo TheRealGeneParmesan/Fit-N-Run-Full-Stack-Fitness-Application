ef167f215b500713ecfe4978ce50ac24cb1d4df6
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-31 15:34:55 -0700

    added media queries for signup

--- client/src/pages/SignupForm.js
+++ client/src/pages/SignupForm.js
@@ -42,54 +42,56 @@ const Signup = () => {
   return (
     <main className="signupPage" style={{ backgroundImage: `url(${backSignup})` }}>
       <div className="col-12 col-lg-10">
-        <div className="card-body">
-          {data ? (
-            <Link to="/"></Link>
-          ) : (
-            <form className="signupForm" onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button
-                className="signupBtn btn btn-block"
-                style={{ cursor: 'pointer' }}
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
-          )}
+        <div className="signupContainer card">
+          <h4 className="signupHeader">Sign Up</h4>
+          <div className="card-body">
+            {data ? (
+              <Link to="/"></Link>
+            ) : (
+              <form className="signupForm" onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your username"
+                  name="username"
+                  type="text"
+                  value={formState.name}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="signupBtn btn btn-block"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-          {error && (
-            <div className="my-3 p-3 bg-danger text-white">
-              {error.message}
-            </div>
-          )}
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    </main >
+    </main>
   );
 };
 
