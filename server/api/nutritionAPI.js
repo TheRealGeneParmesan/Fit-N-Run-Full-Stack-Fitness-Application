7a1cb6ca02cd9359362ad4572405f244c6d9c3e9
twqueen <tiffanyytsai@gmail.com>
2023-06-01 01:28:28 -0700

    trying to add api stuff

--- server/api/nutritionAPI.js
+++ server/api/nutritionAPI.js
@@ -14,3 +14,40 @@ const nutritionData = async (query) => {
 module.exports = {
     nutritionData,
 };
+
+// I DONT THINK THIS IS GONNA WORK SO I GUESS ILL DO RESTLINK IN FRONTEND INSTEAD OF
+// THIS RESTDATASOURCE BS IN THE BACKEND. IM SO TIRED. -Tiff
+
+// const { RESTDataSource } = require("@apollo/datasource-rest");
+
+// // look up nutrition info for any food item
+// class NutritionAPI extends RESTDataSource {
+//     constructor() {
+//         super();
+//         this.baseURL = 'https://trackapi.nutritionix.com/v2/natural/';
+//     }
+
+//     async getNutrientInfo(query) {
+//         return this.post(`nutrients/${query}`, undefined, {
+//             headers: {
+//                 'x-app-id': '2c5cddf1',
+//                 'x-app-key': 'df44732763fd77c6a376ce050adaa721',
+//                 'x-remote-user-id': '0',
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//     }
+
+//     async getExerciseInfo(query) {
+//         return this.post(`exercise/${query}`, undefined, {
+//             headers: {
+//                 'x-app-id': '2c5cddf1',
+//                 'x-app-key': 'df44732763fd77c6a376ce050adaa721',
+//                 'x-remote-user-id': '0',
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//     }
+// }
+
+// module.exports = NutritionAPI;
