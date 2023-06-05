e84ebb51fd8884e165f06789c641ae1de1f9c2f7
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-06-05 11:48:43 -0700

    made cancelation responsive

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://FitnRunAdmin:fitnrunadmin420@fitnruncluster.hsocs29.mongodb.net/?retryWrites=true&w=majority');
-
-// Notes from Geneparmigiano: Need to make sure IP's can connect from anywhere for that access (MongoAtlas)
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/FitnRun');
 
 module.exports = mongoose.connection;
 
-// notes for twqueen
-// useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
-// https://jasonwatmore.com/mongoose-mongodb-fix-for-mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported
+
